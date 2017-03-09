7833770d0f34f0e2bdd3510a23683cf6afeb1450
rincew1nd <xkorikx@gmail.com>
2017-03-09 17:51:27 +0300

    Интерфейс и огромная бага которая всё блокирует

---
+++ Decoder/DecryptorViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Decoder.Annotations;
+
+namespace Decoder
+{
+    public class DecryptorViewModel
+    {
+        public ObservableCollection<Alphabet> Alphabet { get; set; }
+
+        public DecryptorViewModel()
+        {
+            Alphabet = new ObservableCollection<Alphabet>();
+            for (int i = 'А'; i < 'Я'; i++)
+                Alphabet.Add(new Alphabet((char)i, (char)i));
+        }
+    }
+
+    public class Alphabet : INotifyPropertyChanged
+    {
+        private char _decrypted;
+
+        public char Encrypted { get; set; }
+        public char Decrypted
+        {
+            get { return _decrypted; }
+            set
+            {
+                _decrypted = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Alphabet(char e, char d)
+        {
+            Encrypted = e;
+            Decrypted = d;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Decoder/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+
+namespace Decoder
+{
+    public partial class MainWindow
+    {
+        private readonly DecryptorViewModel _decryptorViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _decryptorViewModel = new DecryptorViewModel();
+            DataContext = _decryptorViewModel;
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var encryptedText = new TextRange(EncryptedText.Document.ContentStart, EncryptedText.Document.ContentEnd).Text;
+
+            foreach (var alphabet in _decryptorViewModel.Alphabet)
+                encryptedText = encryptedText.Replace(alphabet.Encrypted, alphabet.Decrypted);
+
+            DecryptedText.Document.Blocks.Clear();
+            DecryptedText.Document.Blocks.Add(new Paragraph(new Run(encryptedText)));
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
